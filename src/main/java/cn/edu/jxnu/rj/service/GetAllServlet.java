c0ac0cdfbfadd80a1ae21c3090d9067b120b9386
liurongfei <63947081+liurongfei@users.noreply.github.com>
2020-12-10 20:10:45 +0800

    ajax 和json测试

---
+++ src/main/java/cn/edu/jxnu/rj/service/GetAllServlet.java
@@ -0,0 +1,28 @@
+package cn.edu.jxnu.rj.service;
+
+import cn.edu.jxnu.rj.dao.UserDao;
+import cn.edu.jxnu.rj.dao.impl.UserDaoImpl;
+import cn.edu.jxnu.rj.domain.User;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "GetAllServlet",urlPatterns = "/getAll")
+public class GetAllServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UserDao userDao = new UserDaoImpl();
+        User user = userDao.findById(2);
+        Gson gson = new Gson();
+        String json = gson.toJson(user);
+        response.getWriter().write(json);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
